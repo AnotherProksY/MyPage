e4349e51ccb35496f50455accbd0527557c7e169
Fazilov Kamil <k.fazilov@icloud.com>
2021-03-21 05:01:04 +0300

    Move assets directory back to root for README

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """This module contains tests for 'src/mypage.py'."""
 
-import sys
-sys.path.append('src')
-
 from webtest import TestApp
 import mypage
+
+import sys
+sys.path.append('src')
 
 
 def test_main_page():
@@ -13,7 +13,7 @@ def test_main_page():
     app = TestApp(mypage.app)
 
     # Check if 'main page' returns '200'
-    assert app.get('/').status == '200 OK'  # nosec
+    assert app.get('/').status == '200 OK'
 
     # Check if we can't access static files from URI
-    assert app.get('/static', expect_errors=True).status == '404 Not Found'  # nosec
+    assert app.get('/static', expect_errors=True).status == '404 Not Found'
